aaf4acbc68291b329afd5219ecaac585050f3dce
ViniciusAdati <138039184+ViniciusAdati@users.noreply.github.com>
2023-10-02 21:59:44 -0300

    Create SWD.py

---
+++ SWD.py
@@ -0,0 +1,30 @@
+import openai
+
+openai_api_key = "sua-chave-da-api-openai"
+
+# Defina a lista de usuários com a estrutura de dados desejada
+users = [
+    {"name": "User1"},
+    {"name": "User2"},
+    {"name": "User3"},
+]
+
+def generate_ai_news(user):
+    completion = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": "Você é um especialista em marketing bancário."
+            },
+            {
+                "role": "user",
+                "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
+            }
+        ]
+    )
+    return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+    news = generate_ai_news(user)
+    print(news)
